60849ed16e508872add4b415a58391e773fe8f64
Cosmin Ilincuta <ilincuta.cosmin@gmail.com>
2022-03-22 16:31:06 +0200

    added image upload functionality

--- src/app/actors/form-actor/form-actor.component.ts
+++ src/app/actors/form-actor/form-actor.component.ts
@@ -24,12 +24,17 @@ export class FormActorComponent implements OnInit {
       name: ['', {
         validators: [Validators.required]
       }],
-      dateOfBirth: ''
+      dateOfBirth: '',
+      picture: ''
     });
 
     if (this.model !== undefined) {
       this.form.patchValue(this.model);
     }
+  }
+
+  onImageSelected(image) {
+    this.form.get('picture').setValue(image);
   }
 
   saveChanges() {
